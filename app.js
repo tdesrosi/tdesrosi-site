fc1e39804183e157139b0b3ce086f9a59add63f4
Thomas Desrosiers <thomasdesrosiers@Thomass-MacBook-Air.local>
2020-05-07 19:54:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port:3000");
+});
